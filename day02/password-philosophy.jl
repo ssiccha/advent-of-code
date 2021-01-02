5125f98d9d168af7ea67242b1d2722bc5b6eb3ed
Sergio Siccha <sergio.siccha@posteo.net>
2021-01-02 23:20:55 +0100

    day2-julia

--- day02/password-philosophy.jl
+++ day02/password-philosophy.jl
@@ -1,10 +1,12 @@
-#!/bin/env julia
-function part1()
+password_philosophy = function(part)
+    if !(part in [1, 2])
+        error("wrong part")
+    end
     passwords = open(f -> read(f, String), "passwords")
     passwords = split(passwords, "\n")
     pop!(passwords)
     re = r"(\d*)-(\d*) (\w): (\w*)"
-    invalid_passwords = []
+    valid_passwords = []
     for i in range(1, stop = length(passwords))
         password = passwords[i]
         m = match(re, password)
@@ -12,15 +14,30 @@ function part1()
         max = parse(Int, m[2])
         restricted_char = m[3][1]
         password_string = m[4]
-        count_restricted_chars = length(filter(x -> x == restricted_char,
-                                               password_string))
-        if count_restricted_chars < min || count_restricted_chars > max
-            push!(invalid_passwords, i)
-            print("invalid password found: count ", count_restricted_chars,
-                  ", min ", min,
-                  ", max ", max,
-                  ", pw ", password, "\n")
+        if part == 1
+            pw_criterium = part1
+        else part == 2
+            pw_criterium = part2
+        end
+        if pw_criterium(min, max, restricted_char, password_string)
+            push!(valid_passwords, i)
         end
     end
-    return length(invalid_passwords)
+    return length(valid_passwords)
+end
+
+part1 = function(min, max, restricted_char, password_string)
+    count_restricted_chars = length(filter(x -> x == restricted_char,
+                                           password_string))
+    print("valid pw: ", restricted_char,
+          ", ", min,
+          " < ", count_restricted_chars,
+          " < ", max,
+          ", pw ", password_string, "\n")
+    return (count_restricted_chars < min || count_restricted_chars > max)
+end
+
+part2 = function (min, max, restricted_char, password_string)
+    return xor(password_string[min] == restricted_char,
+           password_string[max] == restricted_char)
 end
